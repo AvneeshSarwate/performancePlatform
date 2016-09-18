e916dcb4cd043e52073005bf68a3a02919612f48
AvneeshSarwate <avneeshsarwate@gmail.com>
2016-09-17 19:42:58 -0500

    pydal arpeggiator: (draft) forwarding on notes complete

--- arpeggiator.py
+++ arpeggiator.py
@@ -14,7 +14,14 @@ class Arpeggiator:
 	def handle(self, note, vel, onOff):
 		if onOff == "on":
 			self.onNotes[note] = vel
-			#send note
+
+			msg = OSC.OSCMessage()
+			msg.setAddress("/forwardNotes")
+			msg.append(note)
+			msg.append(vel)
+			msg.append(onOff)
+			self.pydal.superColliderClient.sendMsg(msg)
+
 			if len(self.onOntes) == 1:
 				self.channel.play(self.pattern)
 		if onOff == "off" and note in self.onNotes:
@@ -22,7 +29,14 @@ class Arpeggiator:
 			if len(self.onNotes) == 0:
 				self.channel.stop()
 			#send note
-		print self.onNotes 
+		print self.onNotes
 
+	def sendNoteUpdate(self, note, vel onOff):
+		msg = OSC.OSCMessage()
+		msg.setAddress("/forwardNotes")
+		msg.append(note)
+		msg.append(vel)
+		msg.append(onOff)
+		self.pydal.superColliderClient.sendMsg(msg)
 
 	def kill(self):
