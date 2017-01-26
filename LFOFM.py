9cd46e32d72f5d029f5416cee43aa362903ce0b9
Avneesh Sarwate <avneeshsarwate@gmail.com>
2017-01-26 02:13:32 -0500

    renders strings properly under light testing

--- LFOFM.py
+++ LFOFM.py
@@ -1,5 +1,6 @@
-# sn cs sq tr sw
-# s  c  q  t  w 
+# sin cos sqr tri saw
+# sn  cs  sq  tr  sw
+# s   c   q   t   w 
 
 
 # s( q(b) + 4 ) * b 
@@ -10,7 +11,7 @@
 # s = Sin(freq, phase, amp, shift)
 # s2 = s*3 + 60
 # tr1 = Tri(1, s2, 10, 30)
-
+#sin(theta*t+phi)*x + y
 
 class Wave(object):
 
@@ -26,23 +27,27 @@ class Wave(object):
 		return self.waveName + ".(" + ",".join(map(str, valArgs)) + ")"
 
 	def __mul__(self, other):
-		if type(other) is int or type(other) is float:
-			self.amp = self.amp * other
-			self.shift = self.shift * other
+		if type(other) is int or type(other) is float or issubclass(type(other),  Wave):
+			print str(other), self.amp, self.shift
+			self.amp = 0 if other == 0 or self.amp == 0else self.amp * other
+			self.shift = 0 if other == 0 or self.shift == 0 else self.shift * other
+			print str(other), self.amp, self.shift
 			return self
 		else:
 			raise NotImplementedError
 
 	def __rmul__(self, other):
-		if type(other) is int or type(other) is float:
-			self.amp = self.amp * other
-			self.shift = self.shift * other
+		if type(other) is int or type(other) is float or issubclass(type(other),  Wave):
+			self.amp = 0 if other == 0 else self.amp * other
+			self.shift = 0 if other == 0 else self.shift * other
 			return self
 		else:
 			raise NotImplementedError
 
 	def __div__(self, other):
-		if type(other) is int or type(other) is float:
+		if other == 0:
+			raise ValueError("Can't divide by 0")
+		if type(other) is int or type(other) is float or issubclass(type(other),  Wave):
 			self.amp = self.amp / other
 			self.shift = self.shift / other
 			return self
@@ -50,21 +55,21 @@ class Wave(object):
 			raise NotImplementedError
 
 	def __add__(self, other):
-		if type(other) is int or type(other) is float:
+		if type(other) is int or type(other) is float or issubclass(type(other),  Wave):
 			self.shift = self.shift + other
 			return self
 		else:
 			raise NotImplementedError
 
 	def __radd__(self, other):
-		if type(other) is int or type(other) is float:
+		if type(other) is int or type(other) is float or issubclass(type(other),  Wave):
 			self.shift = self.shift + other
 			return self
 		else:
 			raise NotImplementedError
 
 	def __sub__(self, other):
-		if type(other) is int or type(other) is float:
+		if type(other) is int or type(other) is float or issubclass(type(other),  Wave):
 			self.shift = self.shift - other
 			return self
 		else:
@@ -72,31 +77,30 @@ class Wave(object):
 
 
 class Sin(Wave):
-
-	def __init__(self, freq=1, phase=1, amp=1, shift=1):
+	def __init__(self, freq=1, phase=1, amp=1, shift=0):
 		super(Sin, self).__init__(freq, phase, amp, shift)
 		self.waveName = "sinwav"
 
-class Cos(Wave):
 
-	def __init__(self, freq=1, phase=1, amp=1, shift=1):
+class Cos(Wave):
+	def __init__(self, freq=1, phase=1, amp=1, shift=0):
 		super(Cos, self).__init__(freq, phase, amp, shift)
 		self.waveName = "coswav"
 
-class Tri(Wave):
 
-	def __init__(self, freq=1, phase=1, amp=1, shift=1):
+class Tri(Wave):
+	def __init__(self, freq=1, phase=1, amp=1, shift=0):
 		super(Tri, self).__init__(freq, phase, amp, shift)
 		self.waveName = "triwav"
 
-class Sqr(Wave):
 
-	def __init__(self, freq=1, phase=1, amp=1, shift=1):
+class Sqr(Wave):
+	def __init__(self, freq=1, phase=1, amp=1, shift=0):
 		super(Sqr, self).__init__(freq, phase, amp, shift)
 		self.waveName = "sqrwav"
 
-class Saw(Wave):
 
-	def __init__(self, freq=1, phase=1, amp=1, shift=1):
+class Saw(Wave):
+	def __init__(self, freq=1, phase=1, amp=1, shift=0):
 		super(Saw, self).__init__(freq, phase, amp, shift)
 		self.waveName = "sawwav"
