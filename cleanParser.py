5fe33185f956b070d6a5bc538a47b1dc5452b0d0
Avneesh Sarwate <avneeshsarwate@gmail.com>
2017-02-16 17:55:19 -0500

    starting to get max sampling pydal working

--- cleanParser.py
+++ cleanParser.py
@@ -50,6 +50,8 @@ def isPydalSymbol(s):
     return allIn(s, string.letters+string.digits+":"+"~") and s[0] != ":" and s[-1] != ":"
 def isSampleSymbol(s):
     return re.match('^[a-g][a-g]:[0-9](\.[0-9]+)?_[0-9](\.[0-9]+)?$', s) or s == "~"
+def isMaxSymbol(s):
+    return re.match('^[a-d]:[0-9](\.[0-9]+)?(_[0-9](\.[0-9]+)?)?$', s) or s == "~"
 def isArpeggiatorSymbol(s):
     return s.isdigit()
 
@@ -58,6 +60,7 @@ symbolMatchers = {}
 symbolMatchers['pydal'] = isPydalSymbol
 symbolMatchers['sample'] = isSampleSymbol
 symbolMatchers['arp'] = isArpeggiatorSymbol
+symbolMatchers['max'] = isMaxSymbol
 
 
 def parse(inputStr, symbolKey = 'pydal'):
